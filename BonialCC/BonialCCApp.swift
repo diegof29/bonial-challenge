fc46f97e4d813ce7acadebdc2a565f580252b22a
Diego Pais <diego.pais29@gmail.com>
2023-05-16 14:43:41 +0200

    Initial Commit

---
+++ BonialCC/BonialCCApp.swift
@@ -0,0 +1,17 @@
+//
+//  BonialCCApp.swift
+//  BonialCC
+//
+//  Created by Diego Pais on 16.05.23.
+//
+
+import SwiftUI
+
+@main
+struct BonialCCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
